f6ae4327f8467d1971755478acf430c29e699ae1
jdhuf <91219279+jdhuf@users.noreply.github.com>
2022-04-08 13:26:46 -0600

    pushed card, deck, rank, suit

---
+++ src/com/skilldistillery/blackjack/BlackJackApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.blackjack;
+
+public class BlackJackApplication {
+	public static void main(String [] args) {
+		
+		BlackJackApplication app = new BlackJackApplication();
+		app.run();
+	}
+	
+	public void run() {
+		
+	}
+}
